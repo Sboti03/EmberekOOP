63271eb32355d5e2aaa6866f0083828924d24a99
avpboy <somogyibotond03@gmail.com>
2022-09-15 10:34:58 +0200

    Emberek+

--- src/hu/botond/emberekoop/Main.java
+++ src/hu/botond/emberekoop/Main.java
@@ -6,12 +6,10 @@ import hu.botond.emberekoop.filemanagement.FileReader;
 
 public class Main {
     public static void main(String[] args) {
-        Ember szemely = new Ember("Somogyi Botond", "2003. 09. 26.", "Budapest");
-        System.out.println(szemely);
-        System.out.println(szemely.getAge());
 
 
         FileReader fr = new FileReader();
         Emberek szemelyek = new Emberek(fr.ReadInEmbers("emberek.txt"));
+        System.out.println(szemelyek);
     }
 }
