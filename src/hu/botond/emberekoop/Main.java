787f66a8f75b088a6f4f2201147e861fb0794f93
avpboy <somogyibotond03@gmail.com>
2022-09-15 10:48:23 +0200

    Emberek+

--- src/hu/botond/emberekoop/Main.java
+++ src/hu/botond/emberekoop/Main.java
@@ -1,6 +1,5 @@
 package hu.botond.emberekoop;
 
-import hu.botond.emberekoop.ember.Ember;
 import hu.botond.emberekoop.ember.Emberek;
 import hu.botond.emberekoop.filemanagement.FileReader;
 
@@ -9,7 +8,8 @@ public class Main {
 
 
         FileReader fr = new FileReader();
-        Emberek szemelyek = new Emberek(fr.ReadInEmbers("emberek.txt"));
+        Emberek szemelyek = new Emberek(fr.ReadInEmbers("C:\\Petrik\\AMAF_Java\\EmberekOOP\\src\\hu\\botond\\emberekoop\\emberek.txt"));
+
         System.out.println(szemelyek);
     }
 }
--- src/hu/botond/emberekoop/filemanagement/FileReader.java
+++ src/hu/botond/emberekoop/filemanagement/FileReader.java
@@ -13,18 +13,21 @@ public class FileReader {
 
     public Ember[] ReadInEmbers(String path) {
         List<Ember> datas = new ArrayList<Ember>();
-        try {
-            File file = new File(path);
-            Scanner sc = new Scanner(file);
 
-            while (sc.hasNextLine()){
-                String[] data = sc.nextLine().split("-");
-                datas.add(new Ember(data[0], data[1], data[3]));
+            File file = new File(path);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (sc.hasNextLine()){
+                String[] data = sc.nextLine().split(";");
+                datas.add(new Ember(data[0], data[1], data[2]));
             }
 
-        } catch (FileNotFoundException e) {
 
-        }
         Ember[] szemelyek = new Ember[datas.size()];
         szemelyek = datas.toArray(szemelyek);
         return szemelyek;
