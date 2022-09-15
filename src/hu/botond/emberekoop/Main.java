79c60515c2163a04e68d1725e341b41563316f5f
avpboy <somogyibotond03@gmail.com>
2022-09-15 09:24:54 +0200

    Ember osztály

--- src/hu/botond/emberekoop/Main.java
+++ src/hu/botond/emberekoop/Main.java
@@ -1,7 +1,10 @@
 package hu.botond.emberekoop;
 
+import hu.botond.emberekoop.ember.Ember;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ember szemely = new Ember("Somogyi Botond", "2003. 09. 26.", "Budapest");
+        System.out.println(szemely);
     }
 }
